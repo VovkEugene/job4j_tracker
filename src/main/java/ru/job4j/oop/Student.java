be29cc9bcef040305d50ed96543eeb572b43b78f
Eugene Vovk <evgeniyvovk2009@yandex.ru>
2024-12-02 17:43:18 +0300

    1.1. Создание объекта [#165452 #225536], 1.2. Вызов метода объекта [#166085 #225548]

---
+++ src/main/java/ru/job4j/oop/Student.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+public class Student {
+    public void music() {
+        System.out.println("Tra tra tra");
+    }
+
+    public static void main(String[] args) {
+        Student petya = new Student();
+        petya.music();
+        petya.music();
+        petya.music();
+    }
+}
