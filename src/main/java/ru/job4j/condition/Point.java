1ec1d09fec331cd46b42fdba30b7ea2f56f4a042
Eugene Vovk <evgeniyvovk2009@yandex.ru>
2024-12-15 06:09:52 +0300

    4. Расстояние между точками в трехмерном пространстве. [#122643 #225750]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -4,22 +4,58 @@ import static java.lang.Math.pow;
 import static java.lang.Math.sqrt;
 
 public class Point {
+
+    /**
+     * Это поле объекта. Оно доступно только конкретному объекту.
+     */
     private int x;
+
+    /**
+     * И это поле объекта. Оно доступно только конкретному объекту.
+     */
     private int y;
 
+    /**
+     * И это поле объекта. Оно доступно только конкретному объекту.
+     */
+    private int z;
+
+    /**
+     * Конструктор, который принимает начальное состояние объекта "точка"
+     *
+     * @param first  координата x
+     * @param second координата y
+     */
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
+    }
+
+    /**
+     * Конструктор, который принимает начальное состояние
+     * объекта "точка" в трехмерном пространстве
+     *
+     * @param first  координата x
+     * @param second координата y
+     * @param third  координата z
+     */
+    public Point(int first, int second, int third) {
+        this.x = first;
+        this.y = second;
+        this.z = third;
     }
 
     public double distance(Point that) {
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
-    public static void main(String[] args) {
-        Point a = new Point(0, 0);
-        Point b = new Point(0, 2);
-        double distance = a.distance(b);
-        System.out.println(distance);
+    public void info() {
+        System.out.println("Point[" + this.x + ", " + this.y + "]");
+    }
+
+    public double distance3d(Point that) {
+        return Math.sqrt(Math.pow(this.x - that.x, 2)
+                + Math.pow(this.y - that.y, 2)
+                + Math.pow(this.z - that.z, 2));
     }
 }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -1,7 +1,10 @@
 package ru.job4j.condition;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.within;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PointTest {
@@ -70,5 +73,55 @@ class PointTest {
         Point second = new Point(x2, y2);
         double actual = first.distance(second);
         assertEquals(expected, actual, DELTA);
+    }
+
+    @Test
+    @DisplayName("Проверка расстояния между одной и той же точкой (ожидается 0)")
+    void whenDistance3dSamePoint() {
+        Point point1 = new Point(1, 2, 3);
+        Point point2 = new Point(1, 2, 3);
+        double expected = 0;
+        double actual = point1.distance3d(point2);
+        assertThat(actual).isCloseTo(expected, within(0.01));
+    }
+
+    @Test
+    @DisplayName("Проверка расстояния между двумя разными точками в трехмерном пространстве")
+    void whenDistance3dDifferentPoints() {
+        Point point1 = new Point(1, 2, 3);
+        Point point2 = new Point(4, 6, 8);
+        double expected = 7.07;
+        double actual = point1.distance3d(point2);
+        assertThat(actual).isCloseTo(expected, within(0.01));
+    }
+
+    @Test
+    @DisplayName("Проверка расстояния между точками с отрицательными координатами")
+    void whenDistance3dNegativeCoordinates() {
+        Point point1 = new Point(-1, -2, -3);
+        Point point2 = new Point(-4, -6, -8);
+        double expected = 7.07;
+        double actual = point1.distance3d(point2);
+        assertThat(actual).isCloseTo(expected, within(0.01));
+    }
+
+    @Test
+    @DisplayName("Проверка расстояния между точками с нулевыми и положительными координатами")
+    void whenDistance3dMixedCoordinates() {
+        Point point1 = new Point(0, 0, 0);
+        Point point2 = new Point(3, 4, 5);
+        double expected = 7.07;
+        double actual = point1.distance3d(point2);
+        assertThat(actual).isCloseTo(expected, within(0.01));
+    }
+
+    @Test
+    @DisplayName("Проверка расстояния между точками, отличающимися только по одной оси")
+    void whenDistance3dSingleAxisDifference() {
+        Point point1 = new Point(1, 1, 1);
+        Point point2 = new Point(1, 1, 5);
+        double expected = 4;
+        double actual = point1.distance3d(point2);
+        assertThat(actual).isCloseTo(expected, within(0.01));
     }
 }
