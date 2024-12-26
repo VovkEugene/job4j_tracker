28c620b0e9c5202eba85f8f475f16d8de66c5fff
Eugene Vovk <evgeniyvovk2009@yandex.ru>
2024-12-26 12:41:26 +0300

    1. Singleton [#94606 #228541]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,46 @@
+package ru.job4j.tracker;
+
+public class SingleTracker {
+    private static volatile SingleTracker instance;
+
+    private final Tracker tracker;
+
+    private SingleTracker() {
+        tracker = new Tracker();
+    }
+
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            synchronized (SingleTracker.class) {
+                if (instance == null) {
+                    instance = new SingleTracker();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public void delete(int id) {
+        tracker.delete(id);
+    }
+}
