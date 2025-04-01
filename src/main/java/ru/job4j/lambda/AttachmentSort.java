246260362d88d6f686704678e0e1b7a34428f2ec
Eugene Vovk <evgeniyvovk2009@yandex.ru>
2025-04-01 22:47:56 +0300

    1.1. Анонимные классы [#214142 #232051]

---
+++ src/main/java/ru/job4j/lambda/AttachmentSort.java
@@ -0,0 +1,19 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class AttachmentSort {
+    public static void main(String[] args) {
+        List<Attachment> attachments = Arrays.asList(
+                new Attachment("image 1", 100),
+                new Attachment("image 2", 34),
+                new Attachment("image 3", 13)
+        );
+        Comparator<Attachment> comparator =
+                Comparator.comparingInt(Attachment::getSize);
+        attachments.sort(comparator);
+        System.out.println(attachments);
+    }
+}
